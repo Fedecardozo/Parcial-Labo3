3626317993a0dd5c94e5fabf3068e4a75fd54c07
Fedecardozo <Fedecardozo.a@gmail.com>
2023-12-03 12:30:55 -0300

    Borro una funcion que no uso

--- client/js/Monstruo.js
+++ client/js/Monstruo.js
@@ -7,26 +7,3 @@ export class Monstruo extends Personaje {
     this.defensa = defensa;
   }
 }
-
-export const CargarAtributos = (obj, key, value) => {
-  switch (key) {
-    case "id":
-      obj.id = value;
-      break;
-    case "nombre":
-      obj.nombre = value;
-      break;
-    case "tipo":
-      obj.tipo = value;
-      break;
-    case "alias":
-      obj.alias = value;
-      break;
-    case "miedo":
-      obj.miedo = value;
-      break;
-    case "defensa":
-      obj.defensa = value;
-      break;
-  }
-};
